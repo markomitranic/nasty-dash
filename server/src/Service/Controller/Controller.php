f9cca79abce2b994d2f0164fe717c83082bc6d87
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-13 23:48:36 +0200

    Allow for inheriting logging for all api requests inside controller

--- server/src/Service/Controller/Controller.php
+++ server/src/Service/Controller/Controller.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace NastyDash\Service\Controller;
 
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
 
 abstract class Controller
@@ -13,6 +14,33 @@ abstract class Controller
 	public function __construct(LoggerInterface $logger)
 	{
 		$this->logger = $logger;
+	}
+
+	/**
+	 * Borrowed from Httpfoundation's Request toString
+	 * @param ServerRequestInterface $request
+	 */
+	protected function logRequest(ServerRequestInterface $request): void
+	{
+		$content = $request->getBody();
+
+		$cookieHeader = '';
+		$cookies = [];
+		foreach ($request->getCookieParams() as $k => $v) {
+			$cookies[] = $k.'='.$v;
+		}
+		if (!empty($cookies)) {
+			$cookieHeader = 'Cookie: '.implode('; ', $cookies)."\r\n";
+		}
+
+		$this->logger->critical(sprintf(
+			'%s %s %s',
+			$request->getMethod(),
+			$request->getUri(),
+			json_encode($request->getHeaders()).
+				 $cookieHeader."\r\n".
+				 $content
+		));
 	}
 
 }
