dace7384ad18135d5487d4a9c503a3deccb6b74a
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-18 17:57:03 +0200

    Code style improvements

--- server/src/Service/Stats/DateRange/Daily.php
+++ server/src/Service/Stats/DateRange/Daily.php
@@ -1,12 +1,11 @@
 <?php
-
 declare(strict_types=1);
-
 
 namespace NastyDash\Service\Stats\DateRange;
 
-
 class Daily extends Aggregate
 {
+
+	protected string $period = 'P1D';
 
 }
--- server/src/Service/Stats/DateRange/Hourly.php
+++ server/src/Service/Stats/DateRange/Hourly.php
@@ -1,10 +1,7 @@
 <?php
-
 declare(strict_types=1);
 
-
 namespace NastyDash\Service\Stats\DateRange;
-
 
 class Hourly extends Aggregate
 {
