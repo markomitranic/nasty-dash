041cd5df4ee28216ef28bbfe89d4b8f28554e643
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-19 20:19:35 +0200

    Refactor app entrypoint to be easily readable and extendible

--- client/src/scripts/app.js
+++ client/src/scripts/app.js
@@ -4,14 +4,16 @@ import TotalDashboard from "./Dashboard/TotalDashboard";
 import AggregateFormDispatcher from "./Aggregate/FormDispatcher";
 import TotalApiAdapter from "./API/Total/ApiAdapter";
 
+// Register Utilities
 const notificationPopup = new NotificationPopup();
+const aggregateFormDispatcher = new AggregateFormDispatcher(document.getElementById('aggregate-form'));
 
+// Register API adapters
 const totalApiAdapter = new TotalApiAdapter(notificationPopup);
+aggregateFormDispatcher.addObserver(totalApiAdapter);
 
-const aggregateFormDispatcher = new AggregateFormDispatcher(
-	document.getElementById('aggregate-form'),
-	totalApiAdapter
-);
-
+// Build up the router or dashboard sets
 const totalDashboard = new TotalDashboard(totalApiAdapter);
+
+// Trigger first load
 aggregateFormDispatcher.submit(null);
