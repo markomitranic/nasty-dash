6216bdb687421b6a88c25919acb45262573012bf
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-19 21:44:48 +0200

    Just a small tiny bit of styling

--- client/src/scripts/Widgets/Charts/SingleDatapoint.js
+++ client/src/scripts/Widgets/Charts/SingleDatapoint.js
@@ -9,8 +9,19 @@ class SingleDatapoint {
 	}
 
 	update(data) {
+		if (this.isFloat(data)) {
+			data = data.toLocaleString('en-US', {minimumFractionDigits: 2});
+		}
 		this.datapoint = data;
-		this.wrapper.innerHTML = `${this.title}: ${this.datapoint}`;
+		this.wrapper.innerHTML = this.getTemplate(this.title, this.datapoint);
+	}
+
+	getTemplate(title, datapoint) {
+		return `<p>${title}</p><p class="datapoint">${datapoint}</p>`;
+	}
+
+	isFloat(n){
+		return Number(n) === n && n % 1 !== 0;
 	}
 
 }
