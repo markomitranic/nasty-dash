83e085bcbde774f3b0a3e7e0ea2ecf97c8880907
Marko Mitranic <marko.mitranic@gmail.com>
2020-09-20 13:37:48 +0200

    Always optimize autoloader (no reason not to)

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 cd /app/src
 echo "[Waiting] Installing vendors."
-composer install
+composer install --optimize-autoloader
 
 while ! mysqladmin ping --host=${MYSQL_HOST} --user=${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} --silent; do
 	echo "[Waiting] Still waiting for MySQL to get ready."
